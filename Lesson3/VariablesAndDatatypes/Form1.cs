2a913709ea18df5def59a384781bfd24ded6a399
unknown <blue10@RM02-10.fasfg.local>
2015-06-22 12:03:22 +0100

    VariablesAndDatatypes

---
+++ Lesson3/VariablesAndDatatypes/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariablesAndDatatypes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //String hello;
+            //hello = "hello world";
+            //MessageBox.Show(hello);
+            //MessageBox.Show("hello");
+            
+            String firstTextBox = textBox1.Text;
+            String secondTextBox = textBox2.Text;
+
+            label3.Text = firstTextBox + " " + secondTextBox;
+        }
+    }
+}
